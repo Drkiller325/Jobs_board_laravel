7b2fb8a023db6d442b4454042c738df5f9344ab8
Drkiller325 <ahmed-2008-04@hotmail.com>
2025-08-28 20:18:22 +0300

    add migrations and database and populate

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,43 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job {
-    public static function all(): array // return type explicit to array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => "$50,000",
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => "$100,000",
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => "$30,000",
-            ]
-        ];
+class Job extends Model
+{
+    protected $table = "job_listings";
 
-    }
-
-    public static function find($id): array
-    {
-        // Arr class to interact with arrays first gets the first item
-        // of an array that matches a criteria/ finds the first element that matches the
-        // given id in the list of arrays
-        $job = Arr::first(Job::all(), fn($job) => $job['id'] == $id);
-
-        if (!$job)
-        {
-            abort(404);
-        }
-
-        return $job;
-    }
+    protected $fillable = ['title', 'salary'];
 }
--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,11 @@ Route::get('/', function () {
 Route::get('/jobs', function () {
     return view('jobs',[
             'jobs' => Job::all()
-        ]
-
-    );
+        ]);
 });
 
 Route::get('/jobs/{id}', function ($id){
     $job = Job::find($id);
-
-
-
-
     return view('job',['job' => $job]);
 });
 
