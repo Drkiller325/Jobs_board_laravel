eab1ac7afd5b4a179e0529722e44716baa908f27
Drkiller325 <ahmed-2008-04@hotmail.com>
2025-09-04 15:14:43 +0300

    add edit/delete functionality

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,9 @@ Route::get('/', function () {
     return view('home');
 });
 
+//index
 Route::get('/jobs', function () {
     // use latest() to display the jobs by time stamp from latest
-    //
     $jobs = Job::with('employer')->latest()->simplePaginate(3);
 
     return view('jobs.index',[
@@ -17,16 +17,19 @@ Route::get('/jobs', function () {
         ]);
 });
 
+//create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
 // Needs to be close to the bottom because {id} includes anything that comes after 'jobs/'
+//show
 Route::get('/jobs/{id}', function ($id){
     $job = Job::find($id);
     return view('jobs.show',['job' => $job]);
 });
 
+//store
 Route::post('/jobs', function () {
    request()->validate([
        'title' => ['required', 'min:3'],
@@ -43,6 +46,43 @@ Route::post('/jobs', function () {
     return redirect('/jobs');
 });
 
+//edit
+Route::get('/jobs/{id}/edit', function ($id){
+    $job = Job::find($id);
+    return view('jobs.edit',['job' => $job]);
+});
+
+//update
+Route::patch('/jobs/{id}', function ($id){
+    // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required']
+    ]);
+    // authorize (on hold...)
+    // update
+    $job = Job::findOrFail($id);
+
+//    $job->title = request('title');
+//    $job->salary = request('salary');
+//    $job->save();
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    // redirect to the job page
+    return redirect('/jobs/' . $job->id);
+});
+
+Route::delete('/jobs/{id}', function ($id){
+    //authorize (on hold)
+    // delete the job
+    Job::findOrFail($id)->delete();
+
+    return redirect('/jobs');
+});
 
 
 Route::get('/contact', function () {
