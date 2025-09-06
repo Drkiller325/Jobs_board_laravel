2d4fd29eb65ec4d7aea0f24a836c8a748d9765a7
Drkiller325 <ahmed-2008-04@hotmail.com>
2025-09-06 14:32:59 +0300

    implements auth forms and views, refactor some view elements

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -30,6 +32,13 @@ Route::view('/contact', 'contact');
 //});
 
 Route::resource('/jobs' , JobController::class);
+
+//Auth
+Route::get('/register', [RegisterController::class, 'create']);
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
 
 
 
